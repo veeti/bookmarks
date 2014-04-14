0a6cdd2da688b887a3ef1f5a36e38f976817f6e2
Veeti Paananen <veeti.paananen@rojekti.fi>
2014-04-14 04:07:15 +0300

    Fix indentation

--- schema/schema.sql
+++ schema/schema.sql
@@ -1,15 +1,15 @@
 CREATE TABLE users (
-	id SERIAL PRIMARY KEY,
-	username VARCHAR(32) NOT NULL UNIQUE,
-	password_hash VARCHAR(60)
+  id SERIAL PRIMARY KEY,
+  username VARCHAR(32) NOT NULL UNIQUE,
+  password_hash VARCHAR(60)
 );
 
 CREATE TABLE links (
-	id SERIAL PRIMARY KEY,
-	user_id INTEGER REFERENCES users(id),
-	title VARCHAR(256),
-	url VARCHAR(2048),
-	note TEXT,
+  id SERIAL PRIMARY KEY,
+  user_id INTEGER REFERENCES users(id),
+  title VARCHAR(256),
+  url VARCHAR(2048),
+  note TEXT,
   added TIMESTAMP
 );
 
