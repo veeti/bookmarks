62f81a22bf5047bf6544d0aeff925a93efcde6fa
Veeti Paananen <veeti.paananen@rojekti.fi>
2014-06-17 19:47:50 +0300

    Shuffle stuff around more

--- router.js
+++ router.js
@@ -11,24 +11,27 @@ exports.route = function(app) {
     res.render('index.html');
   });
 
-  // Links
   var links = require('./controllers/links');
+  var users = require('./controllers/users');
+
+  // Link browsing
   app.get('/links', authUser, getDatabase, links.links);
   app.get('/links/tag/:tag', authUser, getDatabase, links.links);
 
+  // Link submission and modification
   app.get('/links/new', authUser, getDatabase, links.newLink);
   app.post('/links/new', authUser, getDatabase, links.newLink);
   app.get('/links/:linkId', authUser, getDatabase, authLink, links.editLink);
   app.post('/links/:linkId', authUser, getDatabase, authLink, links.editLink);
   app.get('/links/:linkId/delete', authUser, getDatabase, authLink, links.deleteLink);
   app.post('/links/:linkId/delete', authUser, getDatabase, authLink, links.deleteLink);
+
+  // Bookmarklet
   app.get('/bookmarklet/add', authUser, getDatabase, links.bookmarklet);
   app.post('/bookmarklet/add', authUser, getDatabase, links.bookmarklet);
-
   app.get('/bookmarklet', links.bookmarkletInfo);
 
   // Log in & sign up
-  var users = require('./controllers/users');
   app.get('/login', getDatabase, users.login);
   app.post('/login', getDatabase, users.postLogin);
   app.get('/register', getDatabase, users.register);
