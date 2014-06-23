7a21fdfb442d31f163f8a25dd00838e032c3ee15
Veeti Paananen <veeti.paananen@rojekti.fi>
2014-06-23 20:18:24 +0300

    Slide delete animation

--- static/js/links.index.js
+++ static/js/links.index.js
@@ -6,7 +6,7 @@ $(function() {
     if (confirm('Are you sure you want to delete this link?')) {
       $.post('/links/' + id + '/delete', function(result) {
         if (result) {
-          $('[data-id=' + id + ']').fadeOut(1000);
+          $('[data-id=' + id + ']').slideUp(500);
         }
       });
     }
